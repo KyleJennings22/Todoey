63a46c481065f404af03793610bfd6588ddf230b
Kyle Jennings <Kyle22@gmail.com>
2019-07-08 19:56:19 -0700

    convert to MVC design patter

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Kyle Jennings on 7/8/19.
+//  Copyright © 2019 Kyle Jennings. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+    
+    
+}
